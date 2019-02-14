f8bddb267d4e840751d785215f2052ee952fc5a6
mahineymar <42170082+mahineymar@users.noreply.github.com>
2019-02-14 10:41:18 +0530

    Update maad.py

--- maad.py
+++ maad.py
@@ -1,7 +1,8 @@
 n=int(input(" "))
-if (n%2)==0:
-    print("even")
-elif(n%2)==1:
-    print("odd")
+if(n>0):
+    if (n%2)==0:
+        print("even")
+    elif(n%2)==1:
+        print("odd")
 else:
     print("invalid")
